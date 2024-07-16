48467466c3a2cc153f49ad121da7d05587f87aa3
marcotheo <marcobutalid989@gmail.com>
2024-07-17 00:14:12 +0800

    qwik app deployment setup

--- sst.config.ts
+++ sst.config.ts
@@ -1,12 +1,26 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+import { frontend } from "./infra/frontend";
+
 export default $config({
   app(input) {
     return {
       name: "community-connect",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
+      providers: {
+        aws: {
+          profile: process.env.PROFILE,
+          region: process.env.AWS_REGION as any,
+        },
+      },
     };
   },
-  async run() {},
+  async run() {
+    const result = frontend();
+
+    return {
+      ...result,
+    };
+  },
 });
