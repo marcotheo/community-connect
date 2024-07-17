41ea1b489847ba772f6bddc675e92e324268c7b8
marcotheo <marcobutalid989@gmail.com>
2024-07-17 23:35:52 +0800

    initialize Button component

---
+++ packages/frontend/src/components/button/button.tsx
@@ -0,0 +1,39 @@
+import { component$, Slot, ButtonHTMLAttributes } from "@builder.io/qwik";
+import { cn } from "~/common/utils";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: "lg" | "md" | "default";
+  variant?: "outline" | "ghost" | "default";
+}
+
+export default component$<ButtonProps>(
+  ({ size = "default", variant = "default", ...props }) => {
+    const sizes = {
+      lg: "py-4 px-7",
+      md: "py-3 px-6",
+      default: "py-2 px-5",
+    };
+
+    const variants = {
+      outline: "bg-transparent border border-primary hover:bg-primary-soft",
+      ghost: "bg-primary-soft hover:brightness-150",
+      default: "bg-primary hover:bg-primary-foreground text-white",
+    };
+
+    return (
+      <button
+        {...props}
+        class={cn(
+          "font-primary",
+          "rounded-sm",
+          "duration-150 ease-linear",
+          sizes[size],
+          variants[variant],
+          props.class,
+        )}
+      >
+        <Slot />
+      </button>
+    );
+  },
+);
