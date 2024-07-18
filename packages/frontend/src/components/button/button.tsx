fb117e0b82a16c4ca39f175216d269d63b7edc96
marcotheo <marcobutalid989@gmail.com>
2024-07-18 15:36:12 +0800

    added new button size

--- packages/frontend/src/components/button/button.tsx
+++ packages/frontend/src/components/button/button.tsx
@@ -2,7 +2,7 @@ import { component$, Slot, ButtonHTMLAttributes } from "@builder.io/qwik";
 import { cn } from "~/common/utils";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  size?: "lg" | "md" | "default";
+  size?: "lg" | "md" | "sm" | "default";
   variant?: "outline" | "ghost" | "default";
 }
 
@@ -11,6 +11,7 @@ export default component$<ButtonProps>(
     const sizes = {
       lg: "py-4 px-7",
       md: "py-3 px-6",
+      sm: "py-1 px-4 text-md",
       default: "py-2 px-5",
     };
 
