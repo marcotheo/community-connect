ebd2fb2750be436285c0480ce38e04e726eff49d
marcotheo <marcobutalid989@gmail.com>
2024-07-19 22:23:50 +0800

    change initial page

--- packages/frontend/src/routes/index.tsx
+++ packages/frontend/src/routes/index.tsx
@@ -1,21 +1,6 @@
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { component$ } from "@builder.io/qwik";
 
-import Menu, {
-  DropDownMenuItem,
-  DropDownMenuLabel,
-  DropDownSeparator,
-} from "~/components/dropdownmenu/dropdownmenu";
-import Card, {
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "~/components/card/card";
-import Dialog, { DialogTrigger } from "~/components/dialog/dialog";
-import Heading from "~/components/heading/heading";
-import Button from "~/components/button/button";
-import Input from "~/components/input/input";
-
 export default component$(() => {
   return (
     <div>
