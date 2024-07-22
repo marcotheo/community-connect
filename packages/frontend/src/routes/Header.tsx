6a8f5bc1208fb5edbc303d162f706f1f72e501fc
marcotheo <marcobutalid989@gmail.com>
2024-07-22 23:05:18 +0800

    updated header component

--- packages/frontend/src/routes/Header.tsx
+++ packages/frontend/src/routes/Header.tsx
@@ -1,33 +1,21 @@
-import DarkMode from "~/components/dark-mode/dark-mode";
-import Button from "~/components/button/button";
-import Drawer from "~/components/drawer/drawer";
 import { component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 import Image from "~/media/logo.png?jsx";
-import { cn } from "~/common/utils";
 
-const HamburgerIcon = component$(() => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      class="size-6 bg-transparent"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-      />
-    </svg>
-  );
-});
+import {
+  HamburgerIcon,
+  SignInIcon,
+  SignUpIcon,
+} from "~/components/icons/icons";
+import Drawer, { DrawerLink } from "~/components/drawer/drawer";
+import DarkMode from "~/components/dark-mode/dark-mode";
+import Heading from "~/components/heading/heading";
+import Button from "~/components/button/button";
+import { cn } from "~/common/utils";
 
 const MobileMenu = component$(() => {
   return (
-    <Drawer>
+    <Drawer defaultValue>
       <HamburgerIcon q:slot="trigger" />
 
       <div q:slot="header">
@@ -36,9 +24,25 @@ const MobileMenu = component$(() => {
             <Image />
           </div>
           <div class="flex flex-col justify-center font-bold italic text-xl">
-            <p class="">Genesis</p>
-            <p class="">Oppurtunities</p>
+            <p>Genesis</p>
+            <p>Oppurtunities</p>
           </div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <Heading size="xs" class="text-zinc-600 text-md pl-2">
+            AUTHENTICATION
+          </Heading>
+          <DrawerLink href="/sign-in">
+            <SignInIcon />
+            Sign In
+          </DrawerLink>
+          <DrawerLink href="/sign-up">
+            <SignUpIcon />
+            Sign Up
+          </DrawerLink>
         </div>
       </div>
     </Drawer>
@@ -48,19 +52,19 @@ const MobileMenu = component$(() => {
 export default component$(() => {
   return (
     <div class={cn("flex justify-between items-center", "w-full py-5")}>
-      <div class="flex gap-3">
-        <div class="w-14 h-14">
+      <div class="flex items-center gap-3">
+        <div class="w-10 h-10 sm:w-14 sm:h-14">
           <Image />
         </div>
-        <div class="flex flex-col justify-center font-bold italic text-xl">
-          <p class="">Genesis</p>
-          <p class="">Oppurtunities</p>
+        <div class="flex flex-col justify-center font-bold italic text-base sm:text-xl">
+          <p>Genesis</p>
+          <p>Oppurtunities</p>
         </div>
       </div>
 
       <MobileMenu />
 
-      <div class="flex gap-5 items-center max-sm:hidden">
+      <div class="flex gap-5 items-center max-md:hidden">
         <Link href="/sign-in">
           {" "}
           <Button variant="outline" size="md">
