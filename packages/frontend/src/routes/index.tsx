f43c64455900d5bca287dc8dfba66fdadf559c13
marcotheo <marcobutalid989@gmail.com>
2024-07-29 15:59:40 +0800

    sample image

--- packages/frontend/src/routes/index.tsx
+++ packages/frontend/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import type { DocumentHead } from "@builder.io/qwik-city";
+import CdnImage from "~/components/cdn-image/cdn-image";
 import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
     <div>
       <br />
-      <div class="flex flex-col gap-3">asdasd</div>
+      <div class="flex flex-col gap-3">Testing Optimized IMage</div>
+      <CdnImage filename="sample" width={600} height={600} alt="sample" />
     </div>
   );
 });
